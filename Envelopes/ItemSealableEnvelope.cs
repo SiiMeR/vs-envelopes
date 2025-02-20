1b5500b47c7f92c58e47860f8468d89c1ee7a978
Siim Raudsepp <siimraudsepp@msn.com>
2025-02-20 03:19:39 +0200

    Add recipes to convert envelopes to different states

--- Envelopes/ItemSealableEnvelope.cs
+++ Envelopes/ItemSealableEnvelope.cs
@@ -1,7 +1,26 @@
-﻿using Vintagestory.API.Common;
+﻿using System.Collections.Generic;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Util;
 
 namespace Envelopes;
 
-public class ItemSealableEnvelope: Item
+public class ItemSealableEnvelope : Item
 {
+    public override WorldInteraction[] GetHeldInteractionHelp(ItemSlot inSlot)
+    {
+        var worldInteractions = new List<WorldInteraction>();
+
+        var code = inSlot.Itemstack.Collectible.Code.Path;
+        if (code is "envelope-unsealed" or "envelope-sealed")
+        {
+            worldInteractions.Add(new WorldInteraction
+            {
+                ActionLangCode = "heldhelp-open",
+                MouseButton = EnumMouseButton.Right
+            });
+        }
+
+        return worldInteractions.ToArray().Append(base.GetHeldInteractionHelp(inSlot));
+    }
 }
